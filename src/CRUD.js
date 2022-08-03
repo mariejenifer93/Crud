ec3c568e5a8cf6b2b1b20cefa089a34f7738521f
Marie Jenifer <mariejenifer93>
2022-08-03 19:04:58 +0530

    crud 3aug

--- src/CRUD.js
+++ src/CRUD.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+//URL
+import { API_URL } from "./Constants/URL";
 // Rgex 
 import { passwordValidator } from "./rgex";
 // Textfield 
@@ -12,64 +14,119 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-
+//axios
+import axios from "axios";
+//toast
+import { ToastContainer, toast } from 'react-toastify';
 
 function Crud() {
 
-    const [input, setInput] = useState({ username: '', password: '', phone: '' });
+    // const [input, setInput] = useState({ username: '', password: '', phone: '' });
+
+    const [userName, setuserName] = useState("");
+    const [password, setpassword] = useState("");
+    const [phone, setphone] = useState("");
+    const [id, setId] = useState('');
+
     const [userNameErr, setuserNameErr] = useState("");
     const [passwordErr, setpasswordErr] = useState("");
     const [phoneErr, setphoneErr] = useState("");
 
-    const handvalue = (e) => {
-        setInput({ ...input, [e.target.name]: e.target.value });
+
+    // const handvalue = (e) => {
+    //     setInput({ ...input, [e.target.name]: e.target.value });
+    // }
+
+    const [apiData, setAPIData] = useState([]);
+
+    const callGetAPI = async () => {
+        const resp = await axios.get(API_URL);
+        setAPIData(resp.data);
     }
+
+    const deleteUser = async (id) => {
+        await axios.delete(`${API_URL}/${id}`);
+        toast.error('Successful Delete', { autoClose: 3000 })
+        callGetAPI();
+    }
+
+    const updateUser = ({ id, userName, password, phone }) => {
+        localStorage.setItem('id', id)
+        localStorage.setItem('userName', userName)
+        localStorage.setItem('password', password)
+        localStorage.setItem('phone', phone)
+    }    
+
+    useEffect(() => {
+        callGetAPI();
+        setId(localStorage.getItem('id'))
+        setuserName(localStorage.getItem('userName'))
+        setpassword(localStorage.getItem('password'))
+        setphone(localStorage.getItem('phone'))
+    }, []);
+    
+
+    const updatevalue = async () =>{
+        await axios.put(`${API_URL}/${id}` , {
+            userName,
+            password,
+            phone
+        })
+    }
+
 
     const savefield = (e) => {
         e.preventDefault();
         validation();
     }
 
-    const validation = () => {
-        if (input.username) {
-            setuserNameErr('');            
+    const validation = async () => {
+        if (userName) {
+            setuserNameErr('');
         } else {
-            setuserNameErr('Name Field is required');           
+            setuserNameErr('Name Field is required');
         }
-        // if(!passwordValidator(input.password)){
-        //     setpasswordErr('password should have minimum 8 character with the combination of uppercase,lowsercase, numbers and specialcharaters')
-        // } else{
-        //     setpasswordErr('')
-        // }  
-        const cond1 = /(?=.*?[A-Z])/;
-        const cond2 = /(?=.*?[a-z])/;
-        const cond3 = /(?=.*?[0-9])/;
-        const cond4 = /(?=.*?[#?!@$%^&*-])/;
-        if(!(input.password)){
-            setpasswordErr('Password Field is required');  
-        } else if((input.password).length < 4){
-            setpasswordErr('Too short');  
-        }else if((input.password).length >= 10){
-            setpasswordErr('Too long');
-        }else if(!(input.password).match(cond1)){
-            setpasswordErr('Uppercase Missing');
-        }else if(!(input.password).match(cond2)){
-            setpasswordErr('Lowercase Missing');
-        }else if(!(input.password).match(cond3)){
-            setpasswordErr('Number Missing');
-        }else if(!(input.password).match(cond4)){
-            setpasswordErr('Special character Missing');
-        }else{
-            setpasswordErr('');
-        }       
+        if (!passwordValidator(password)) {
+            setpasswordErr('password should have minimum 8 character with the combination of uppercase,lowsercase, numbers and specialcharaters')
+        } else {
+            setpasswordErr('')
+        }
+        // const cond1 = /(?=.*?[A-Z])/;
+        // const cond2 = /(?=.*?[a-z])/;
+        // const cond3 = /(?=.*?[0-9])/;
+        // const cond4 = /(?=.*?[#?!@$%^&*-])/;
+        // if (!password) {
+        //     setpasswordErr('Password Field is required');
+        // } else if (password.length < 4) {
+        //     setpasswordErr('Too short');
+        // } else if (password.length >= 10) {
+        //     setpasswordErr('Too long');
+        // } else if (!password.match(cond1)) {
+        //     setpasswordErr('Uppercase Missing');
+        // } else if (!password.match(cond2)) {
+        //     setpasswordErr('Lowercase Missing');
+        // } else if (!password.match(cond3)) {
+        //     setpasswordErr('Number Missing');
+        // } else if (!password.match(cond4)) {
+        //     setpasswordErr('Special character Missing');
+        // } else {
+        //     setpasswordErr('');
+        // }
 
-        if (input.phone) {           
+        if (phone) {
             setphoneErr('');
-        } else {           
+        } else {
             setphoneErr('Phone Number Field is required');
         }
-    }
+        if (userName !== "" && password !== "" && phone !== "") {
+            await axios.post(API_URL, {
+                userName,
+                password,
+                phone
+            })
+        }
 
+    }
     return (
         <div className="container">
             <Button variant="contained" className='mt-5 mb-5' data-toggle="modal" data-target="#myModal">Create</Button>
@@ -83,60 +140,39 @@ function Crud() {
                         <div className="modal-body">
                             <div className="mb-4">
                                 <TextField
-                                    id="username"
-                                    name="username"
-                                    label="UserName"
-                                    type="text"
-                                    fullWidth
-                                    placeholder="Enter your name"
-                                    InputLabelProps={{
-                                        shrink: true,
-                                    }}
-                                    variant="standard"
-                                    className="inputfield"
-                                    onChange={handvalue}
-                                    onFocus = {(e)=>{setuserNameErr('')}}
+                                    id="UserName" name="UserName" label="UserName" type="text" fullWidth placeholder="Enter your name"
+                                    InputLabelProps={{ shrink: true, }}
+                                    variant="standard" autoComplete="off" className="inputfield" value={userName}
+                                    onChange={(e) => { setuserName(e.target.value) }}
+                                    onFocus={(e) => { setuserNameErr('') }}
+                                    inputProps={{ maxLength: 12 }}
                                 />
                                 <div style={{ color: 'red', marginTop: '10px' }}>{userNameErr}</div>
                             </div>
                             <div className="mb-4">
                                 <TextField
-                                    id="password"
-                                    name="password"
-                                    label="Password"
-                                    type="text"
-                                    fullWidth
-                                    placeholder="Enter your Password"
-                                    InputLabelProps={{
-                                        shrink: true,
-                                    }}
-                                    variant="standard"
-                                    className="inputfield"
-                                    onChange={handvalue}
-                                    onFocus = {(e)=>{setpasswordErr('')}}
+                                    id="password" name="password" label="Password" type="text" fullWidth placeholder="Enter your Password"
+                                    InputLabelProps={{ shrink: true, }}
+                                    variant="standard" autoComplete="off" className="inputfield" value={password}
+                                    onChange={(e) => { setpassword(e.target.value) }}
+                                    onFocus={(e) => { setpasswordErr('') }}
                                 />
                                 <div style={{ color: 'red', marginTop: '10px' }}>{passwordErr}</div>
                             </div>
                             <div className="mb-4">
                                 <TextField
-                                    id="phone"
-                                    name="phone"
-                                    label="Phone Number"
-                                    type="number"
-                                    fullWidth
-                                    placeholder="Enter Your Phone Number"
-                                    InputLabelProps={{
-                                        shrink: true,
-                                    }}
-                                    variant="standard"
-                                    className="inputfield"
-                                    onChange={handvalue}
-                                    onFocus = {(e)=>{setphoneErr('')}}
+                                    id="phone" name="phone" label="Phone Number" type="tel" fullWidth placeholder="Enter Your Phone Number"
+                                    InputLabelProps={{ shrink: true, }}
+                                    variant="standard" autoComplete="off" className="inputfield" value={phone}
+                                    onChange={(e) => { setphone(e.target.value) }}
+                                    onFocus={(e) => { setphoneErr('') }}
+                                    inputProps={{ maxLength: 12 }}
                                 />
                                 <div style={{ color: 'red', marginTop: '10px' }}>{phoneErr}</div>
                             </div>
                             <div className='text-center'>
                                 <Button variant="contained" color="success" className='mt-3 mb-3' onClick={savefield}>Save</Button>
+                                <Button variant="contained" color="info" className='mt-3 mb-3' onClick={updatevalue}>Update</Button>
                             </div>
                         </div>
                     </div>
@@ -148,28 +184,27 @@ function Crud() {
                         <TableRow>
                             <TableCell>UserName</TableCell>
                             <TableCell>Password</TableCell>
-                            <TableCell>Phone NUmber</TableCell>
+                            <TableCell>Phone Number</TableCell>
+                            <TableCell>Action</TableCell>
+                            <TableCell>Action</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        <TableRow>
-                            <TableCell>1</TableCell>
-                            <TableCell>2</TableCell>
-                            <TableCell>3</TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell>1</TableCell>
-                            <TableCell>2</TableCell>
-                            <TableCell>3</TableCell>
-                        </TableRow>
-                        <TableRow>
-                            <TableCell>1</TableCell>
-                            <TableCell>2</TableCell>
-                            <TableCell>3</TableCell>
-                        </TableRow>
+                        {
+                            apiData.map(data => (
+                                <TableRow key={data.id}>
+                                    <TableCell>{data.userName}</TableCell>
+                                    <TableCell>{data.password}</TableCell>
+                                    <TableCell>{data.phone}</TableCell>
+                                    <TableCell><Button data-toggle="modal" data-target="#myModal" onClick={() => updateUser(data)}>Edit</Button></TableCell>
+                                    <TableCell><Button onClick={() => deleteUser(data.id)}>Delete</Button></TableCell>                                    
+                                </TableRow>
+                            ))
+                        }
                     </TableBody>
                 </Table>
             </TableContainer>
+            <ToastContainer />
         </div>
     )
 }
---
+++ src/Constants/URL.js
@@ -0,0 +1,1 @@
+export const API_URL = 'https://62ea09813a5f1572e872d957.mockapi.io/user'
